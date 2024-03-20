49bf8347266e43bfc09c5d1ee190b60f7f92146d
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-03-20 11:57:10 +0530

    Time: 220 ms (6.40%) | Memory: 129.4 MB (5.07%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,80 +1,71 @@
-#pragma GCC optimize ("O3")
-
 class Solution {
-private:
-    typedef unsigned long long u64_t;
-    typedef unsigned int       u32_t;
-    typedef unsigned short     u16_t;
-    typedef unsigned char      u8_t;
-
-    static constexpr u8_t FWIDTH = 20;
-    static constexpr u64_t FMASK = (1ull << FWIDTH) - 1u;;
-
-    static u64_t setfield(const u64_t v, const u8_t exp, const u32_t value)
-    __attribute__((always_inline)) {
-        return (v & ~(FMASK << exp)) + (u64_t(value) << exp);
-    }
-
 public:
-    static int garbageCollection(vector<string>& garbage, vector<int>& travel) {
-        const u32_t n = travel.size();
-        u64_t ends = 0;
-        u32_t rtime = 0, count = garbage.front().length();
-        for (u32_t i = 1; i <= n; i++) {
-            const string &a = garbage[i];
-            const char *ac = a.c_str();
-            u8_t m = a.length();
-            count += m;
-            if (m >= 8u) {
-                const u64_t vk = *reinterpret_cast<const u64_t*>(ac) - 0x4747474747474747ull,
-                            vexp = ((vk >> 2) & 0x3F3F3F3F3F3F3F3Full) * 20u;
-                ends = setfield(ends, vexp & 0xFFu, i);
-                ends = setfield(ends, (vexp >> 8) & 0xFFu, i);
-                ends = setfield(ends, (vexp >> 16) & 0xFFu, i);
-                ends = setfield(ends, (vexp >> 24) & 0xFFu, i);
-                ends = setfield(ends, (vexp >> 32) & 0xFFu, i);
-                ends = setfield(ends, (vexp >> 40) & 0xFFu, i);
-                ends = setfield(ends, (vexp >> 48) & 0xFFu, i);
-                ends = setfield(ends, (vexp >> 56) & 0xFFu, i);
-                ac += 8u;
-                m -= 8u;
-            }
-            if (m >= 4u) {
-                const u32_t vk = *reinterpret_cast<const u32_t*>(ac) - 0x47474747u,
-                            vexp = ((vk >> 2) & 0x3F3F3F3Fu) * 20u;
-                ends = setfield(ends, vexp & 0xFFu, i);
-                ends = setfield(ends, (vexp >> 8) & 0xFFu, i);
-                ends = setfield(ends, (vexp >> 16) & 0xFFu, i);
-                ends = setfield(ends, (vexp >> 24) & 0xFFu, i);
-                ac += 4u;
-                m -= 4u;
-            }
-            if (m >= 2u) {
-                const u16_t vk = *reinterpret_cast<const u16_t*>(ac) - 0x4747u,
-                            vexp = ((vk >> 2) & 0x3F3Fu) * 20u;
-                ends = setfield(ends, vexp & 0xFFu, i);
-                ends = setfield(ends, (vexp >> 8) & 0xFFu, i);
-                ac += 2u;
-                m -= 2u;
-            }
-            if (m >= 1u) {
-                const u8_t k = *ac - 'G', exp = (k & (k - 1u)) * 5u;
-                ends = setfield(ends, exp, i);
-                ac += 1u;
-                m--;
-            }
-            travel[i-1u] = rtime += travel[i-1u];
-        }
-        const u32_t ep = ends >> FWIDTH * 2u, em = (ends >> FWIDTH) & FMASK, eg = ends & FMASK;
-        return (ep ? travel[ep-1u] : 0u) + (em ? travel[em-1u] : 0u) +
-               (eg ? travel[eg-1u] : 0u) + count;
+    int garbageCollection(vector<string>& g, vector<int>& t) {
+        int c = 1; //time taken to collect
+        int gs = g.size(); //garbage size
+        vector<vector<int>> vec(gs,vector<int> (3,0));
+        vector<int> last(3,-1);
+        // [ G P M]
 
+        for (int i = 0; i < gs; i++)
+        {
+            for (char y: g[i])
+            {
+                if (y == 'G')
+                {
+                    vec[i][0]++;
+                    last[0] = i;
+                }
+                else if (y == 'P')
+                {
+                    vec[i][1]++;
+                    last[1] = i;
+                }
+                else if (y == 'M')
+                {
+                    vec[i][2]++;
+                    last[2] = i;
+                }
+            }
+        }
+
+        int time = 0;
+        for (int i = 0; i < gs; i++)
+        {
+            //garbage truck
+            int flag = 0;
+            if (i <= last[0])
+            {
+                time += vec[i][0];
+                if (i!=last[0])
+                    flag++;
+            }
+            
+            if (i <= last[1]){
+                time += vec[i][1];
+                if (i!=last[1])
+                    flag++;
+            }
+
+            if (i <= last[2])
+            {
+                time += vec[i][2];
+                if (i!=last[2])
+                    flag++;
+            }
+            if (flag == 0)
+            {
+                break;
+            }
+            
+            if (i==gs-1)
+            {
+                break;
+            }
+            time += flag*(t[i]);
+            
+        }   
+
+        return time;
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
