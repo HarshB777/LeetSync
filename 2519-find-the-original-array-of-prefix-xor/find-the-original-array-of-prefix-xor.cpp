2aa128b75b8f8b2ab3d35ccbddd27ffe6612689f
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-03-20 09:47:19 +0530

    Time: 80 ms (46.42%) | Memory: 78.2 MB (94.03%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -8,11 +8,11 @@ public:
             return pref;
         }
 
-        vector<int> ans;
-        ans.push_back(pref[0]);
+        vector<int> ans(n,0);
+        ans[0]=pref[0];
         for (int i = 1; i < n; i++)
         {
-            ans.push_back(pref[i] ^ pref[i-1]);
+            ans[i] = (pref[i] ^ pref[i-1]);
         }
 
         return ans;
