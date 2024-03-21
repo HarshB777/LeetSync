c74e6e662d6172c18a65d33c013d2ef498da9fd2
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-03-21 10:56:45 +0530

    Time: 3 ms (70.13%) | Memory: 10.6 MB (82.99%) - LeetSync

--- 1310-watering-plants/watering-plants.cpp
+++ 1310-watering-plants/watering-plants.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int wateringPlants(vector<int>& plants, int capacity) {
-        int step=0, i=0, temp=capacity;
-        while(i<plants.size()-1){
-            step++;
-            temp-=plants[i];
-            if(temp<plants[i+1]){
-                step+=2*(i+1);
-                temp=capacity;
+        int n = plants.size();
+
+        int src = -1;
+        int bot = capacity;
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (bot >= plants[i])
+            {
+                cnt++;
+                bot -= plants[i];
             }
-            i++;
+
+            else
+            {
+                cnt += (i-src-1);
+                bot = capacity;
+                cnt += (i-src);
+                bot -= plants[i];
+            }
+
+
         }
-        return step+1;
+
+        return cnt;
     }
 };
