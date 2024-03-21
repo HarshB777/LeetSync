3621f30123b1350aad5cf6f03f8185ada95e7b44
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-03-21 09:14:40 +0530

    Time: 11 ms (25.55%) | Memory: 12.8 MB (37.39%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
@@ -1,71 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        int r = mat.size();
-        int c = mat[0].size();
-        int k = r-1;
-        int curr = mat[k][0];
-        vector<vector<int>> ans(r, vector<int> (c));
-        while (k >= 0)
-        {
-            vector<int> temp;
-
-            int z = k;
-            int y = 0;
-
-            while (z<r && y<c)
-            {
-                temp.push_back(mat[z][y]);
-                z++;
-                y++;
+        int rows = mat.size();
+        int cols = mat[0].size();
+        
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> umap;
+        
+        // Fill the map with diagonals
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                umap[i - j].push(mat[i][j]);
             }
-            sort(begin(temp),end(temp));
-
-            z = k;
-            y = 0;
-            int i = 0;
-            while (z<r && y<c)
-            {
-                ans[z][y] = temp[i];
-                i++;
-                z++;
-                y++;
-            }
-
-            k--;
         }
-        int y = 1;
-
-        while (y < c)
-        {
-            vector<int> temp;
-
-            int z = y;
-            int x = 0;
-
-            while (x<r && z<c)
-            {
-                temp.push_back(mat[x][z]);
-                z++;
-                x++;
+        
+        // Update the matrix with sorted values
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                mat[i][j] = umap[i - j].top();
+                umap[i - j].pop();
             }
-            sort(begin(temp),end(temp));
-
-            z = y;
-            x = 0;
-            int i = 0;
-            while (x<r && z<c)
-            {
-                ans[x][z] = temp[i];
-                i++;
-                z++;
-                x++;
-            }
-
-            y++;
         }
-
-
-        return ans;
+        
+        return mat;
     }
 };
