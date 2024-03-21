b7f929c386b221f8b31c5812fcb641c4fb710df4
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-03-21 09:27:23 +0530

    Time: 16 ms (97.25%) | Memory: 84.9 MB (95.79%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        
-        unordered_map<int,int> hm; //val,freq;
-        int n = A.size();
-        vector<int> ans(n,0);
-        int cnt = 0;
-        for (int i = 0; i < n; i++)
-        {
-            hm[A[i]]++;
-            hm[B[i]]++;
-
-            if (hm[A[i]] == 2)
-            {
-                cnt++;
-                hm[A[i]] = 0;
-            }
-
-            if (hm[B[i]] == 2)
-            {
-                cnt++;
-                hm[B[i]] = 0;
-            }
-            ans[i] = cnt;
+        vector<int>freq(A.size()+1,0);
+        vector<int>c(A.size(),0);
+        int count = 0;
+        for(int i=0;i<A.size();i++){
+            freq[A[i]]++;
+            if(freq[A[i]]==2) count++;
+            freq[B[i]]++;
+            if(freq[B[i]]==2) count++;
+            c[i] = count;
         }
-
-
-        return ans;
+        return c;
     }
 };
