457a7bcc152550733737d9640780caf5dde08521
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-03-21 10:41:03 +0530

    Time: 147 ms (95.69%) | Memory: 67.4 MB (92.82%) - LeetSync

--- 1945-finding-the-users-active-minutes/finding-the-users-active-minutes.cpp
+++ 1945-finding-the-users-active-minutes/finding-the-users-active-minutes.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
-        unordered_map<int,set<int>> hm;
-        vector<int> ans(k,0);
+        sort(logs.begin(),logs.end());
+        vector<int> ans(k);
+        int s=logs.size();
+        int l=logs[0][0],lv=-1;
+        int cnt=0;
+        bool flag;
+        for(int i=0;i<s;++i){
+            if(logs[i][0]==l){
+                if(logs[i][1]!=lv ){
+                    cnt++;
+                    lv=logs[i][1];
+                    flag=true;
+                    l=logs[i][0];
+                }
+            }
+            else{
+                ans[cnt-1]++;
+                l=logs[i][0];
+                lv=logs[i][1];
+                cnt=1;
+                flag=false;
+            }
+        }
+        if(flag)
+            ans[cnt-1]++;
+        else
+            ans[cnt-1]++;
 
-        for (auto x: logs)
-        {
-            hm[x[0]].insert(x[1]);
-        }
-        for (auto x: hm)
-        {
-            //cout<<x.first<<' '<<x.second.size()<<endl;
-            ans[x.second.size()-1]++;
-        }
         return ans;
     }
 };
