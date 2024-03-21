8f0a10550a89c047ce7de79951a1a5414ab9f0a7
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-03-21 08:52:42 +0530

    Time: 42 ms (83.05%) | Memory: 34.6 MB (14.36%) - LeetSync

---
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        int r = score.size();
+        int c = score[0].size();
+
+        unordered_map<int,int> hm; //val,idx;
+        vector<int> temp;
+
+        for (int i = 0; i < r; i++)
+        {
+            hm[score[i][k]] = i;
+            temp.push_back(score[i][k]);
+        }
+
+        sort(temp.begin(),temp.end());
+        int last = r-1;
+
+        auto it = hm.end();
+        vector<vector<int>> ans;
+        for (int i = 0; i < r; i++)
+        {
+            vector<int> t;
+            int crow = hm[temp[last]];
+            last--;
+            for (int j = 0; j < c; j++)
+            {
+                t.push_back(score[crow][j]);
+            }
+            ans.push_back(t);
+        }
+
+        return ans;
+    }
+};
