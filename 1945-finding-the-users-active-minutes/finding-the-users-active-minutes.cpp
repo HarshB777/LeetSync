8c207af6b7418bc19e2800c33ab2f0aa1e745f82
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-03-21 10:38:45 +0530

    Time: 217 ms (23.88%) | Memory: 110.2 MB (8.62%) - LeetSync

---
+++ 1945-finding-the-users-active-minutes/finding-the-users-active-minutes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+        map<int,unordered_set<int>> hm;
+        vector<int> ans(k,0);
+
+        for (auto x: logs)
+        {
+            hm[x[0]].insert(x[1]);
+        }
+        for (auto x: hm)
+        {
+            //cout<<x.first<<' '<<x.second.size()<<endl;
+            ans[x.second.size()-1]++;
+        }
+        return ans;
+    }
+};
