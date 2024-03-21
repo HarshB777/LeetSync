61dc8ce05cb1f220222d10fe7a31cad8685339f1
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-03-21 08:28:17 +0530

    Time: 46 ms (57.83%) | Memory: 76.1 MB (18.64%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.cpp
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
-        int n = nums.size();
-        int m = l.size();
-        vector<bool> ans(m,false);
-
-        for (int i = 0; i < m; i++)
+    bool solve(int low,int high,vector<int>nums)
+    {
+        if(low==high) return false;
+        sort(nums.begin()+low,nums.end()+high-nums.size()+1);
+        int count=1;
+        int diff=nums[low+1]-nums[low];
+        for(int i=low+1;i<high;i++)
         {
-            int start = l[i];
-            int end = r[i];
-
-            vector<int> temp;
-            for (int j = start; j <= end; j++)
-            {
-                temp.push_back(nums[j]);
-            }
-            sort(temp.begin(),temp.end());
-            int diff = temp[1] - temp[0];
-            int flag = 1;
-            for (int k = 1; k < temp.size()-1; k++)
-            {
-                if (temp[k+1]-temp[k] != diff)
-                {
-                    flag = 0;
-                    break;
-                }
-            }
-
-            if (flag == 1)
-            {
-                ans[i] = true;
-            }
-
-
+            if((nums[i+1]-nums[i])==diff)
+                count++;
         }
-
-        return ans;
+        if(count==(high-low))
+            return true;
+        return false;
+    }
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        vector<bool>v(l.size(),false);
+        for(int i=0;i<l.size();i++)
+        {
+            if(solve(l[i],r[i],nums))
+                v[i]=true;
+            
+        }
+        return v;
     }
 };
