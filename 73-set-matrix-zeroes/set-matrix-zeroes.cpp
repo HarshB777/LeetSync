2b7bae932f7f5471b620898cff293241d598543d
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-03-23 00:41:56 +0530

    Time: 3 ms (98.74%) | Memory: 16.1 MB (46.68%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& m) {
-        vector<int> v1;
-        vector<int> v2;
+        
 
         int r = m.size();
         int c = m[0].size();
+
+        vector<int> v1(r,0);
+        vector<int> v2(c,0);
 
         for (int i = 0; i < r; i++)
         {
@@ -13,25 +15,20 @@ public:
             {
                 if (m[i][j] == 0)
                 {
-                    v1.push_back(i);
-                    v2.push_back(j);
+                    v1[i] = 1;
+                    v2[j] = 1;
                 }
             }
         }
 
-        for (int x: v1)
+        for (int i = 0; i < r; i++)
         {
             for (int j = 0; j < c; j++)
             {
-                m[x][j] = 0;
-            }
-        }
-
-        for (int x: v2)
-        {
-            for (int i = 0; i < r; i++)
-            {
-                m[i][x] = 0;
+                if (v1[i]==1 || v2[j]==1)
+                {
+                    m[i][j] = 0;
+                }
             }
         }
     }
